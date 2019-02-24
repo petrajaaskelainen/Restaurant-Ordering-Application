047c8dad410cbfc89d9a8a12023644a3dfc73221
Areeb Asad <areebasad95@hotmail.com>
2019-02-24 16:22:08 +0100

    login embeded

--- js/login.js
+++ js/login.js
@@ -15,40 +15,7 @@ $(document).ready(function(){
     
     console.log(userDetails("jorass")); 
     
-    $('#loginbtn').click(function(){
-
-        var userName = $('#loginUsername').val();              
-        var userPass = $('#loginPassword').val()
-
-        var userDet =  userDetails(userName);   // jorass 
-        var hash = md5(userPass);               // "2063c1608d6e0baf80249c42e2be5804"
-
-
-        if (userName === '' || userPass === ''){
-        
-                if(userName === userDet.userName && hash === userDet.pass){
-
-                    console.log("correct");
-
-
-
-                }
-                 else{
-                        $('#loginmsg').text("Username OR Password Incorrect!");
-                 }
-        }
-        else{
-                //if empty then show
-                $('#loginmsg').text("Username OR Password not entered!");
-
-        }
-
-
-
-
-    })  
-
-
+    
     
     
 
--- js/tabchanger.js
+++ js/tabchanger.js
@@ -2,11 +2,43 @@
 
 $(document).ready(function(){
 	
-$("#loginbtn").click(function(){
-    $("#login").hide();
-	$("#checkout").show();
-  });
-	
+	$('#loginbtn').click(function(){
+
+		var userName = $('#loginUsername').val();              
+		var userPass = $('#loginPassword').val();
+
+	 
+
+		if (userName != '' || userPass != ''){
+		
+				var userDet =  userDetails(userName);   // jorass 
+				var hash = md5(userPass);               // "2063c1608d6e0baf80249c42e2be5804"
+
+
+						if(userName === userDet.userName && hash === userDet.pass){
+
+							$("#login").hide();
+							$("#checkout").show();
+
+
+
+						}
+						 else{
+										$('#loginMsg').text("Username OR Password Incorrect!");
+						 }
+		}
+		else{
+						//if empty then show
+						$('#loginMsg').text("Username OR Password not entered!");
+
+		}
+
+
+
+
+}); 
+
+
 $("#checkoutbtn").click(function(){
 	$("#progress").show();
 	$("#content").hide();
