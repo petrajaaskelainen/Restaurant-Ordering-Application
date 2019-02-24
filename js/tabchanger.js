026fce3ce58028cf47e7442bab77ba5f69cf32f4
Areeb Asad <areebasad95@hotmail.com>
2019-02-24 16:53:12 +0100

    some commenting

--- js/tabchanger.js
+++ js/tabchanger.js
@@ -4,38 +4,29 @@ $(document).ready(function(){
 	
 	$('#loginbtn').click(function(){
 
-		var userName = $('#loginUsername').val();              
-		var userPass = $('#loginPassword').val();
+		var userName = $('#loginUsername').val();   //  Get username from input field (html)         
+		var userPass = $('#loginPassword').val();		//  Get password from input field (html)
 
 	 
 
-		if (userName != '' || userPass != ''){
+		if (userName != '' || userPass != ''){ 			// Make sure username and password entered by user are not empty 
 		
-				var userDet =  userDetails(userName);   // jorass 
-				var hash = md5(userPass);               // "2063c1608d6e0baf80249c42e2be5804"
-
+				var userDet =  userDetails(userName);   // Get user details from database 
+				var hash = md5(userPass);               // Convert plain text password to md5 hash.
 
 						if(userName === userDet.userName && hash === userDet.pass){
-
 							$("#login").hide();
 							$("#checkout").show();
-
-
-
 						}
 						 else{
-										$('#loginMsg').text("Username OR Password Incorrect!");
+							 			// if username or password is not correct then display this text
+										$('#loginMsg').text("Username OR Password Incorrect!");	
 						 }
 		}
 		else{
 						//if empty then show
 						$('#loginMsg').text("Username OR Password not entered!");
-
 		}
-
-
-
-
 }); 
 
 
