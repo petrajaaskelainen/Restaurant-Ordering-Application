371af58935d717df4dd56aa9e53d05cc67af33bc
areebasad <areebasad95@hotmail.com>
2019-03-17 14:05:08 +0100

    Update drop.js

--- js/drop.js
+++ js/drop.js
@@ -105,7 +105,7 @@ function drop(ev) {
    
 
     let backgroundColor = $(nodeID).css('background');  // store original background
-    $(nodeID).css('background', 'yellow');              // change element background
+    $(nodeID).css('background', 'SpringGreen');              // change element background
      setTimeout(function() {
         $(nodeID).css('background', backgroundColor);   // change it back after ...
      }, 600);  
@@ -297,7 +297,23 @@ function removeItemFromCart(cartItemID,addInUndo = true){
     return data;
 }
 
+function emptyCartView(){
 
+    removeCartItemListeners();
+
+    let cart = JSON.parse(sessionStorage.getItem(CART));
+    
+    cart = {"items": []};
+    
+    sessionStorage.setItem(CART, JSON.stringify(cart));
+
+
+    $("#checkoutCart").empty();
+
+    $("#checkoutTotal").text("Total: " +  "0" + " kr.");
+   
+
+}
 
 
 
@@ -322,10 +338,10 @@ function drawCartView(){
                 + 'data-cart-listing-price="' + cartPrice + '" ' 
                 + 'data-quantity="' + cartQuantity + '" >'
                 + '<span class="name">' + cartName + '</span>'
-                + '<span class="price">' + cartPrice * cartQuantity + '</span>' 
                 + '<span class="cartPlusButtonSpan">' + "<button class='cartItemsPlusButton'>"+ "+"+'</button>' +'</span>'
                 + '<span class="quantity">'+ cartQuantity +'</span>'
                 + '<span class="cartMinusButtonSpan">'+ "<button class='cartItemsMinusButton'>"+ "-"+'</button>' +'</span>'
+                + '<span class="price">' + cartPrice * cartQuantity + '</span>' 
                 + '<span class="cartRemoveSpan">'+ "<button class='cartItemsRemoveButton'>"+ "X"+'</button>' +'</span>'
                 + '</div>';
     
@@ -387,6 +403,13 @@ function addCartItemListeners(){
             minusQuantityOfItemInCart(cartID);
             drawCartView();
         }  
+
+    })
+
+    /*Listens if empty button of cart item is clicked.*/
+    $('#emptyCartBtn').on('click', function(){
+       
+        emptyCartView();
 
     })
 
